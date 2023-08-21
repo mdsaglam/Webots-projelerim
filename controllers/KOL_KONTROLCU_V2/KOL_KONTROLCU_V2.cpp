d15b1dabf5a223a2e32219b3e3bbcc289dc2d47b
Musa Davut SAĞLAM <mdsaglam@outlook.com>
2023-08-21 17:44:59 +0300

    21.08.2023 güncelleme

--- controllers/KOL_KONTROLCU_V2/KOL_KONTROLCU_V2.cpp
+++ controllers/KOL_KONTROLCU_V2/KOL_KONTROLCU_V2.cpp
@@ -31,22 +31,24 @@ int main(int argc, char **argv)
   /* Kütüphane yardımı ile robotun tanımlanması ve kod bloğu içerisinde robotun tanımlanacağı fonksiyonlar için isim ataması */
   Robot *robot = new Robot();
   /* Kütüphane ile motorların tanımlanması ve kod bloğu içerisinde motorların tanımlanacağı fonksiyonlar için isim ataması */
-  Motor *motor[2];
+  Motor *motor[4];
   /* kütüphane ile klavyenin tanımlanması ve kod bloğu içerisinde klavyenin tanımlanacağı fonksiyonlar için isim ataması */
   Keyboard kb;
   /* klavye girdilerinin kontrol edilmesi */
   /* BU BÖLGEDEKİ KODUN YORUM SATIRLARI KAPATILACAK OLURSA KLAVYE GİRDİLERİ KONSOLDAKİ ÇIKTIYA GÖRE DÜZENLENEBİLİR */
   //  char w = 'W'; //87
   //  char s = 'S'; //83
-  //  char d = 'D'; //65
-  //  char a = 'A'; //68
-  // cout << int(w) << endl;
-  // cout << int(s) << endl;
+char q = 'Q'; //65
+char e = 'E'; //68
+ cout << int(q) << endl;
+ cout << int(e) << endl;
   // cout << int(d) << endl;
   // cout << int(a) << endl;
 /* Motorların kütüphane aracılığı ile tanıtılması*/
   motor[0] = robot->getMotor("DIKEY_MOTOR");
   motor[1] = robot->getMotor("YATAY_MOTOR");
+  motor[2] = robot->getMotor("TUTAMAC_SAG");
+  motor[3] = robot->getMotor("TUTAMAC_SOL");
   /* simülasyon zamanının entegre edilmesi*/
   int timeStep = (int)robot->getBasicTimeStep();
   /*klavyenin örnekleme periyodu tanımının yapılması */
@@ -54,10 +56,10 @@ int main(int argc, char **argv)
   /* hız değişkenlerinin ayarlanması (simülasyonda rad/s cinsinden ifade ediliyor)*/
   double solSagHiz = 0.0;
   double yukariAsagiHiz = 0.0;
+  double ileriGeriHiz = 0.0;
 /* kodun tekrarlı çalışmasını sağlayan while kodu*/
   while (robot->step(timeStep) != -1)
   {
-    
 /*klavye girdisi alınması*/
     int key = kb.getKey();
     /*klavye girdilerinin kıyaslanarak hız verilerinin güncellenmesi */
@@ -74,23 +76,37 @@ int main(int argc, char **argv)
     else if (key == 65)
     {
       yukariAsagiHiz = 0.0;
-      solSagHiz = 1.0;
+      solSagHiz = 5.0;
     }
     else if (key == 68)
     {
       yukariAsagiHiz = 0.0;
-      solSagHiz = -1.0;
+      solSagHiz = -5.0;
+    }
+    else if(key == 81)
+    {
+    ileriGeriHiz = -0.1;
+    }
+    else if(key == 69)
+    {
+    ileriGeriHiz = 0.1;
     }
     else
     {
       yukariAsagiHiz = 0.0;
       solSagHiz = 0.0;
+      ileriGeriHiz = 0.0;
     }
     /*motorların konum ve ivme verilerinin güncelenmesi*/
     motor[0]->setPosition(INFINITY);
     motor[1]->setPosition(INFINITY);
+    motor[2]->setPosition(INFINITY);
+    motor[3]->setPosition(INFINITY);
     motor[0]->setVelocity(yukariAsagiHiz);
     motor[1]->setVelocity(solSagHiz);
+    motor[2]->setVelocity(ileriGeriHiz);
+    motor[3]->setVelocity(ileriGeriHiz);
+
 
 
   }
